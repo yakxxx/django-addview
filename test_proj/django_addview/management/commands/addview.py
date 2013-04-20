b664e5a6ccfd44fe112c0f55a0a6911066febb45
yakxxx <yakxxx@gmail.com>
2013-04-20 15:15:35 +0200

    more django views

--- test_proj/django_addview/management/commands/addview.py
+++ test_proj/django_addview/management/commands/addview.py
@@ -24,7 +24,8 @@ class TemplateForm(npyscreen.Form):
             scroll_exit=True,
             max_height=7,
             name='Create template from:',
-            values=self._tpl_creation_choices
+            values=self._tpl_creation_choices,
+            value=0
         )
 
     def afterEditing(self):
@@ -68,7 +69,7 @@ class SingleObjectMixin(object):
             name='slug_url_kwarg',
             begin_entry_at=self.BEGIN_ENTRY
         )
-        self.slug_url_kwarg = self.add(
+        self.pk_url_kwarg = self.add(
             npyscreen.TitleText,
             name='pk_url_kwarg',
             begin_entry_at=self.BEGIN_ENTRY
@@ -80,7 +81,70 @@ class SingleObjectMixin(object):
         )
 
     def _save_parameters(self):
-        pass
+        self._view_params.update(
+            {'model': self.model.value,
+             'queryset': self.queryset.value,
+             'slug_field': self.slug_field.value,
+             'slug_url_kwarg': self.slug_url_kwarg.value,
+             'pk_url_kwarg': self.pk_url_kwarg.value,
+             'context_object_name': self.context_object_name.value}
+        )
+
+
+class MultipleObjectMixin(object):
+    BEGIN_ENTRY = 22
+
+    def create(self):
+        self.allow_empty = self.add(
+            npyscreen.TitleSelectOne,
+            name='allow_empty',
+            values=['True', 'False'],
+            value=0,
+            max_height=2,
+            scroll_exit=True,
+            begin_entry_at=self.BEGIN_ENTRY
+        )
+        self.model = self.add(
+            npyscreen.TitleText,
+            name='model',
+            begin_entry_at=self.BEGIN_ENTRY
+        )
+        self.queryset = self.add(
+            npyscreen.TitleText,
+            name='queryset',
+            begin_entry_at=self.BEGIN_ENTRY
+        )
+        self.paginate_by = self.add(
+            npyscreen.TitleText,
+            name='paginate_by',
+            begin_entry_at=self.BEGIN_ENTRY
+        )
+        self.page_kwarg = self.add(
+            npyscreen.TitleText,
+            name='page_kwarg',
+            begin_entry_at=self.BEGIN_ENTRY
+        )
+        self.paginator_class = self.add(
+            npyscreen.TitleText,
+            name='paginator_class',
+            begin_entry_at=self.BEGIN_ENTRY
+        )
+        self.context_object_name = self.add(
+            npyscreen.TitleText,
+            name='context_object_name',
+            begin_entry_at=self.BEGIN_ENTRY
+        )
+
+    def _save_parameters(self):
+        self._view_params.update(
+            {'model': self.model.value,
+             'allow_empty': self.allow_empty.value,
+             'queryset': self.queryset.value,
+             'paginate_by': self.paginate_by.value,
+             'page_kwarg': self.page_kwarg.value,
+             'paginator_class': self.paginator_class.value,
+             'context_object_name': self.context_object_name.value}
+        )
 
 
 class ViewParamsForm(TemplateForm):
@@ -96,22 +160,28 @@ class DetailViewParamsForm(TemplateForm, SingleObjectMixin):
 
     def _save_parameters(self):
         super(DetailViewParamsForm, self)._save_parameters()
-        self._view_params.update(
-            {'model': self.model.value}
-        )
+        SingleObjectMixin._save_parameters(self)
 
 
 class TemplateViewParamsForm(TemplateForm):
     def create(self):
         super(TemplateViewParamsForm, self).create()
 
+#    def _save_parameters(self):
+#        super(TemplateViewParamsForm, self)._save_parameters()
+#        self._view_params.update(
+#            {'model': self.model.value}
+#        )
 
-class ListViewParamsForm(npyscreen.Form):
+
+class ListViewParamsForm(TemplateForm, MultipleObjectMixin):
     def create(self):
-        self.template_name = self.add(npyscreen.TitleText, name='template_name')
-        self.model = self.add(npyscreen.TitleText, name='model')
-        super(DetailViewParamsForm, self).create()
+        super(ListViewParamsForm, self).create()
+        MultipleObjectMixin.create(self)
 
+    def _save_parameters(self):
+        super(ListViewParamsForm, self)._save_parameters()
+        MultipleObjectMixin._save_parameters(self)
 
 class FunctionViewParamsForm(npyscreen.Form):
     def create(self):
@@ -135,6 +205,7 @@ class ViewTypeForm(npyscreen.Form):
             npyscreen.TitleSelectOne,
             scroll_exit=True,
             max_height=7,
+            value=0,
             name='View Type',
             values=self.choices
         )
@@ -158,9 +229,26 @@ class ViewTypeForm(npyscreen.Form):
 class MyApplication(npyscreen.NPSAppManaged):
     def onStart(self):
         self.addForm('MAIN', ViewTypeForm, name='View Type')
-        self.addForm('ViewParamsForm', ViewParamsForm, name='View Params')
-        self.addForm('DetailViewParamsForm', DetailViewParamsForm, name='DetailView Params')
-        self.addForm('TemplateViewParamsForm', TemplateViewParamsForm, name='TemplateView Params')
+        self.addForm(
+            'ViewParamsForm',
+            ViewParamsForm,
+            name='View Params'
+        )
+        self.addForm(
+            'DetailViewParamsForm',
+            DetailViewParamsForm,
+            name='DetailView Params'
+        )
+        self.addForm(
+            'TemplateViewParamsForm',
+            TemplateViewParamsForm,
+            name='TemplateView Params'
+        )
+        self.addForm(
+            'ListViewParamsForm',
+            ListViewParamsForm,
+            name='ListView Params'
+        )
 
 
 class Command(BaseCommand):
